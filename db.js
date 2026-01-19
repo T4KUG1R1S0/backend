246f804275c760d4f99223470c984b2661141657
T4KUG1R1S0 <Ardayanto08@gmail.com>
2026-01-19 10:07:03 +0700

    fix serverless crash

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
-  throw new Error("MONGODB_URI not defined");
+  throw new Error("MONGODB_URI is not defined");
 }
 
 let cached = global.mongoose;
@@ -16,9 +16,7 @@ async function connectDB() {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
-      bufferCommands: false,
-    }).then((mongoose) => mongoose);
+    cached.promise = mongoose.connect(MONGODB_URI).then((m) => m);
   }
 
   cached.conn = await cached.promise;
